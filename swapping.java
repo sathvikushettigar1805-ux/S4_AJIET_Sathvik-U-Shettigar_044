685921a99f23c9c4f1a2e1e9bc1f83dc03c3bef5
Sathvik U Shettigar <sathvikushettigar1805@gmail.com>
2026-05-09 14:54:31 +0530

    Implement swapping of two integers in Java

---
+++ swapping.java
@@ -0,0 +1,17 @@
+public class swaping {
+    public static void main(String[] args){
+        int a = 11;
+        int b = 10;
+        System.out.println("Before swapping:");
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swaping");
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+    
+}
